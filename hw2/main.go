6df303e348d981389a771325e41157e926ef6cfd
LolaInTa Chi <lolainta@gmail.com>
2023-10-27 16:56:35 +0800

    feat: hw2 idct

--- hw2/main.go
+++ hw2/main.go
@@ -20,6 +20,23 @@ func write_png(filename string, img image.Image) {
 	ofile.Close()
 }
 
+func write_freq(filename string, freq [][]float64) {
+	ofile, err := os.Create(filename)
+	if err != nil {
+		log.Println(err)
+	}
+	w, h := len(freq), len(freq[0])
+	img := image.NewGray(image.Rect(0, 0, w, h))
+	for i := 0; i < w; i++ {
+		for j := 0; j < h; j++ {
+			tmp := 20*math.Log10(math.Abs(freq[i][j])) + 128
+			img.SetGray(i, j, color.Gray{Y: uint8(tmp)})
+		}
+	}
+	png.Encode(ofile, img)
+	ofile.Close()
+}
+
 func gray_scale(img image.Image) image.Image {
 	bounds := img.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
@@ -41,7 +58,7 @@ func C(i int) float64 {
 	return 1
 }
 
-func dct_2d(img image.Image) image.Image {
+func dct_2d(img image.Image) [][]float64 {
 	bounds := img.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
 	if w != h {
@@ -72,17 +89,56 @@ func dct_2d(img image.Image) image.Image {
 			bar.Add(1)
 		}
 	}
-	ret := image.NewGray(bounds)
+	ret := make([][]float64, w)
+	for i := 0; i < w; i++ {
+		ret[i] = make([]float64, h)
+	}
 	for u := 0; u < w; u++ {
 		for v := 0; v < h; v++ {
-			cur := 20*math.Log10(math.Abs(<-c[u*h+v])) + 128
-			ret.SetGray(u, v, color.Gray{Y: uint8(cur)})
+			ret[u][v] = <-c[u*h+v]
 		}
 	}
 	return ret
 }
 
-func dct_1d(img image.Image) image.Image {
+func idct_2d(freq [][]float64) image.Image {
+	w, h := len(freq), len(freq[0])
+	if w != h {
+		log.Println("Image is not square")
+		return nil
+	}
+	c := make([]chan float64, w*h)
+	bar := progressbar.Default(int64(w * h))
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			c[x*h+y] = make(chan float64)
+			go func(x, y int, c chan float64) {
+				ret := 0.0
+				for u := 0; u < w; u++ {
+					for v := 0; v < h; v++ {
+						ret += freq[u][v] *
+							math.Cos((float64(2*x+1)*float64(u)*math.Pi)/
+								(2*float64(w))) *
+							math.Cos((float64(2*y+1)*float64(v)*math.Pi)/
+								(2*float64(h))) *
+							C(u) * C(v)
+					}
+				}
+				c <- ret * 2 / math.Sqrt(float64(w)*float64(h))
+			}(x, y, c[x*h+y])
+			bar.Add(1)
+		}
+	}
+	ret := image.NewGray(image.Rect(0, 0, w, h))
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			cur := <-c[x*h+y]
+			ret.SetGray(x, y, color.Gray{Y: uint8(cur)})
+		}
+	}
+	return ret
+}
+func dct_1d(img image.Image) [][]float64 {
 	bounds := img.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
 	if w != h {
@@ -129,11 +185,13 @@ func dct_1d(img image.Image) image.Image {
 			reg[u*w+v] = <-ch[u]
 		}
 	}
-	ret := image.NewGray(bounds)
-	for u := 0; u < w; u++ {
-		for v := 0; v < h; v++ {
-			cur := 20*math.Log10(math.Abs(reg[u*h+v])) + 128
-			ret.SetGray(u, v, color.Gray{Y: uint8(cur)})
+	ret := make([][]float64, w)
+	for i := 0; i < w; i++ {
+		ret[i] = make([]float64, h)
+	}
+	for i := 0; i < w; i++ {
+		for j := 0; j < h; j++ {
+			ret[i][j] = reg[i*h+j]
 		}
 	}
 	return ret
@@ -155,17 +213,25 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
 	os.Mkdir("output", 0755)
-	log.Println("Start processing...")
+
+	log.Println("Start gray scale...")
 	gray := gray_scale(src)
 	write_png("output/gray.png", gray)
 	log.Println("Gray scale done")
+
 	log.Println("Start 1d dct...")
 	dct1 := dct_1d(gray)
-	write_png("output/dct_1d.png", dct1)
+	write_freq("output/dct_1d_freq.png", dct1)
 	log.Println("1d dct done")
+
 	log.Println("Start 2d dct...")
 	dct2 := dct_2d(gray)
-	write_png("output/dct_2d.png", dct2)
+	write_freq("output/dct_2d_freq.png", dct2)
 	log.Println("2d dct done")
+
+	log.Println("Start 2d idct...")
+	idct2 := idct_2d(dct2)
+	write_png("output/idct_2d.png", idct2)
 }
